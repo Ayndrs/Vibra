2edaa1e5dec7ef1f1dd120dba8c294ada94a8754
Bush <64496558+Ayndrs@users.noreply.github.com>
2023-12-14 15:57:37 -0600

    Songs fetching and list display

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,14 @@
 import Header from "@/components/Header"
 import ListItem from "@/components/ListItem"
+import getSongs from "@/actions/getSongs";
 
-export default function Home() {
+import PageContent from "./components/PageContent";
+
+export const revalidate = 0;
+
+export default async function Home() {
+  const songs = await getSongs();
+
   return (
     <div className="
       bg-neutral-900
@@ -48,7 +55,7 @@ export default function Home() {
           </h1>
         </div>
         <div>
-          List of Songs!
+          <PageContent songs={songs} />
         </div>
       </div>
     </div>
